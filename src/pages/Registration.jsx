a3f9ce382e8f4343a6d7f056802dec36ec293224
Newsun Neupane <123829389+newsunneupane@users.noreply.github.com>
2026-05-15 13:58:23 +0530

    Update API endpoint in Registration component

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch(`http://localhost/backend/login.php`, {
+      const response = await fetch(`http://newsun.kesug.comt/backend/login.php`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
